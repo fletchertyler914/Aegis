c485b1931230a90bba5f3790a458580b51764209
Tyler Fletcher <fletchertyler914@yahoo.com>
2016-10-23 10:55:59 -0400

    Cleanup

--- app/app.comp.js
+++ app/app.comp.js
@@ -81,7 +81,7 @@
             // Call the API to send the Police
             sendPolice(user);
             
-            // Start Timer for cancellation
+            // Start 10 Sec Timer for Police Cancellation
             model.countDown = 10
             model.onTimeout = function(){
                 if (model.countDown > 0) {
@@ -89,9 +89,12 @@
                     model.countDown--;
                 }
                 else {
+                    // If Pin Not entered And Timer = 0
+                    // Remove Cancel Button From View
                     model.cancelDisabled = true;
                 }
             }
+            // Create a variable to interact with the timer (Start/Stop)
             mytimeout = $timeout(model.onTimeout,1000);
             
         }
@@ -145,7 +148,8 @@
             if (pin != user.pin) {
                 alert("Pin Authentication Failed!")
             }
-            else{   
+            else{ 
+                // If Pin Entered Correctly, Cancel Timer and Police
                 resetTimer();
                 cancelPolice();
                 
